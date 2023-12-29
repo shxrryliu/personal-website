0b54243b72c624a5738fd909eec42a6190c147ce
Sherry Liu <sherryliu@Sherrys-MacBook-Pro.local>
2023-12-28 23:51:52 -0800

    started project layout

--- pages/projects.js
+++ pages/projects.js
@@ -3,10 +3,20 @@ import '../tailwind.config'
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-6">
-            <h2 className="text-3xl font-semibold text-center mt-36">Projects.</h2>
+            {/* intro */}
+            <h2 className="text-3xl font-semibold text-center mt-32">Projects.</h2>
             <p className='text-med font-medium text-center pt-3 pb-10'>some of my work that i've particularly enjoyed.</p>
 
-            <div className='flex sm:flex-row flex-col items-center max-w-3xl'>
+            {/* product design */}
+            <div className='max-w-3xl w-full p-4 md:mx-auto mx-3 my-4'>
+                <h3 className='text-xl pb-4 text-gray-800'>product design</h3>
+                <div className='grid grid-cols-1 sm:grid-cols-2 sm:gap-16 gap-4'>
+                    <div className='w-full h-64 bg-gray-300 rounded-lg'></div>
+                    <div className='w-full h-64 bg-gray-300 rounded-lg'></div>
+                </div>
+            </div>
+            <div className='max-w-3xl w-full p-4 md:mx-auto mx-3 my-4'>
+                <h3 className='text-xl pb-4 text-gray-800'>quantitative sociology reports</h3>
             </div>
         </div>
     )
